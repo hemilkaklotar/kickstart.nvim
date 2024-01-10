47889fddc7750138c930b359e127d5052ffa1855
Hemil Kaklotar <83568973+hemilkaklotar@users.noreply.github.com>
2024-01-10 16:49:08 +0530

    added plugins and themes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,25 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+return {
+{
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 999,
+    config = function ()
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
+
+    {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = 'catppuccin',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+
+}
