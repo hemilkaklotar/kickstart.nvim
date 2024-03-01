60f68903c95eba7a3fe0a292686dac8e325e8529
Hemil Kaklotar <83568973+hemilkaklotar@users.noreply.github.com>
2024-03-01 11:39:45 +0530

    removed unwanted plugins and add config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,18 +1,17 @@
 return {
-
   {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = 'auto',
-        -- component_separators = { left = '', right = '' },
-        -- component_separators = '',
-        -- section_separators = { left = '', right = '' },
-        globalstatus = true, -- enable global statusline (have a single statusline
-      },
-    },
+    'folke/tokyonight.nvim',
+    priority = 999,
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    name = 'tokyonight',
+    config = function()
+      require('tokyonight').setup {
+        transparent = true, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+      }
+      vim.cmd.colorscheme 'tokyonight-night'
+      -- You can configure highlights by doing something like
+      vim.cmd.hi 'Comment gui=none'
+    end,
   },
 }
